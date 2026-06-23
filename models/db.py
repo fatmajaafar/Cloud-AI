7b312e15292ad537d9e31fd474e2e59ddf756247
fatmajaafar <60942453+fatmajaafar@users.noreply.github.com>
2026-06-23 17:39:28 +0100

    small fixes

--- models/db.py
+++ models/db.py
@@ -7,3 +7,7 @@ def get_db_connection():
 
 def get_db_connection_dict():
     return psycopg2.connect(Config.DATABASE_URL, cursor_factory=RealDictCursor)
+# Deux types de connexions :
+
+# get_db_connection() → retourne des tuples (valeur1, valeur2, ...) → on accède par index row[0]
+# get_db_connection_dict() → retourne des dictionnaires {'id': 1, 'name': '...'} → on accède par nom row['id']
